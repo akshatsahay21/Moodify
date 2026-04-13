1f7f8794d3267f8fda2b1abb676dabe1a3ef4dde
Akshat Sahay <akshatsahay2106@gmail.com>
2026-04-14 02:01:48 +0530

    deployment

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,11 @@
 import axios from "axios"
 
+// const api = axios.create({
+//     baseURL: "http://localhost:3000",
+//     withCredentials: true
+// })
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://moodify-backend.onrender.com",
     withCredentials: true
 })
 
